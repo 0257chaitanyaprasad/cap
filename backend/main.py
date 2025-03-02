24ae69888523e7e618a8fb135b9f263b53f7fd1c
Kunda Chaitanya Prasad <101545458+0257chaitanyaprasad@users.noreply.github.com>
2025-03-02 13:31:01 +0530

    Create main.py

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Query
+from pydantic import EmailStr
+from email_validator import validate_email, EmailNotValidError
+from typing import Union
+
+app = FastAPI(title="Email Validation API")
+
+@app.get("/")
+def welcome():
+    return {"message": "Email Validation API is Running 🔥"}
+
+@app.get("/validate-email/")
+def validate(email: EmailStr = Query(example="user@gmail.com")):
+    try:
+        email_info = validate_email(email)
+        return {
+            "is_email_valid": True,
+            "email": email_info.email,
+            "domain": email_info.domain
+        }
+    except EmailNotValidError:
+        return {
+            "is_email_valid": False,
+            "email": email,
+            "message": "Invalid Email Address"
+        }
